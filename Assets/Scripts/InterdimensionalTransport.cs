20897d97c3a14d5ad58f3213f3af44fbc13878d6
Lorent Tiku <tsm.lorent@gmail.com>
2024-03-08 10:38:20 +0100

    Portal done

---
+++ Assets/Scripts/InterdimensionalTransport.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+public class InterdimensionalTransport : MonoBehaviour
+{
+    public Material[] materials;
+
+
+  
+    void Start()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        }
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.name != "Main Camera")
+            return;
+
+        if (transform.position.z > other.transform.position.z)
+        {
+            Debug.Log("Outside other world");
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+            }
+
+
+
+        }
+
+        else
+        {
+            Debug.Log("Inside other world");
+            foreach (var mat in materials)
+            {
+                mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+            }
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+    }
+
+    void Update() 
+    {
+        
+    }
+}
